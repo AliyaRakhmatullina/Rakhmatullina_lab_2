a38da4ff571385d585648a8a6b56d64feecea35d
AliyaRakhmatullina <9874208544al@gmail.com>
2025-05-02 23:01:06 +0300

    вынесла класс в отдельный файл

---
+++ lab_2/EntitiesFileContent.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+using lab_2.Entities;
+
+namespace lab_2;
+
+public class EntitiesFileContent
+{
+    [XmlArray]
+    [XmlArrayItem("Bee")]
+    public List<Bee>? Bees { get; set; }
+    [XmlArray]
+    [XmlArrayItem("Beehive")]
+    public List<Beehive>? Beehives { get; set; }
+    [XmlArray]
+    [XmlArrayItem("Apiary")]
+    public List<Apiary>? Apiaries { get; set; }
+}
